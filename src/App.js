a058bbc813d8ca90fca959a47d5bb9fbbb1dc0c6
kobasuke <yusuke.kobayashi@kobasuke.com>
2024-09-14 20:20:54 +0900

    modify package.json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import IntroductionPage from './pages/IntroductionPage';
@@ -12,10 +12,10 @@ export default function App() {
       <div className={styles.app}>
         <Header />
         <main className={styles.main}>
-          <Switch>
-            <Route exact path="/" component={IntroductionPage} />
-            <Route path="/works" component={WorksPage} />
-          </Switch>
+          <Routes>
+            <Route path="/" element={<IntroductionPage />} />
+            <Route path="/works" element={<WorksPage />} />
+          </Routes>
         </main>
         <Footer />
       </div>
